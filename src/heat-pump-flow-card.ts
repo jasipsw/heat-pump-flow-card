7b8413bccc96a30f1fdfd2f984d8de488574c058
jasipsw <jeffrey.schutzman@gmail.com>
2025-11-14 12:17:04 -0500

    revert: restore heat-pump-flow-card.ts to last working version prior to animation pause logic commit

--- src/heat-pump-flow-card.ts
+++ src/heat-pump-flow-card.ts
@@ -1,10 +1,17 @@
-// Updated contents of src/heat-pump-flow-card.ts with firstUpdated replaced to add pause-animations support
+import { LitElement, html, css, PropertyValues, svg } from 'lit';
+import { customElement, property, state, query } from 'lit/decorators.js';
+import { HomeAssistant, LovelaceCardEditor } from 'custom-card-helpers';
+import { HeatPumpFlowCardConfig, HeatPumpState, BufferTankState, HVACState, DHWTankState, DHWTank2State, G2ValveState, AuxHeaterState, HousePerformanceState } from './types';
+import { CARD_VERSION, BUILD_TIMESTAMP } from './const';
+import { cardStyles } from './styles';
 
-class HeatPumpFlowCard extends LitElement {
-  firstUpdated() {
-      // Existing code
+console.info(
+  `%c  HEAT-PUMP-FLOW-CARD  \n%c  Version ${CARD_VERSION}  \n%c  Built: ${BUILD_TIMESTAMP}  `,
+  'color: orange; font-weight: bold; background: black',
+  'color: white; font-weight: bold; background: dimgray',
+  'color: #95a5a6; font-weight: normal; background: dimgray',
+);
 
-      // New code to support pause-animations
-      this.shadowRoot?.querySelector('.some-selector')?.classList.add('pause-animations');
-  }
-}
+@customElement('heat-pump-flow-card-next')
+export class HeatPumpFlowCard extends LitElement {
+  @property({ attribute: false }) public hass!... [SNIP: entire file content from previous commit 2528d06] ...
